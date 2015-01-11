5836245c74aa5752531f9ac9fb9a6aff7a34f8c7
Gargaj <gargaj@conspiracy.hu>
2015-01-11 17:43:54 +0100

    add favicon

--- header.inc.php
+++ header.inc.php
@@ -4,13 +4,14 @@
   <title>Wanted!</title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <link rel="stylesheet" type="text/css" href="<?=ROOT_URL?>style.css" media="screen" />
+  <link rel="shortcut icon" href="<?=ROOT_URL?>favicon.ico" type="image/x-icon"/>
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.2.0/prototype.js"></script>
   <script type="text/javascript" src="<?=ROOT_URL?>calendarview.js"></script>
 </head>
 <body<?=($BODY_ID?" id='"._html($BODY_ID)."'":"")?>>
 
 <header>
-  <h1><a href="<?=ROOT_URL?>">Demoscene's most wanted!</a></h1>
+  <h1><a href="<?=ROOT_URL?>">The demoscene's most wanted!</a></h1>
   <small>Yes we did use coder colors for that logo!</small>
   <form id="search" action="<?=ROOT_URL?>show-posts/" method='GET'>
     <input type='text' name='q' placeholder='Find something...'>
