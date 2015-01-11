028ae7f98d81fd754de67e5030f37d039bc41eb2
Gargaj <gargaj@conspiracy.hu>
2015-01-11 16:56:32 +0100

    rename file

--- _rewrite.php
+++ _rewrite.php
@@ -5,7 +5,7 @@ $rewriter = new Rewriter();
 $rewriter->addRules(array(
   // site
   "^\/+login\/?$" => "login.php",
-  "^\/+show-posts\/?$" => "site_showposts.php",
+  "^\/+show-posts\/?$" => "site_listposts.php",
   "^\/+add-post\/?$" => "site_addpost.php",
   "^\/+post\/(\d+)\/?" => "site_showpost.php?id=$1",
   "^\/+about\/?$" => "site_about.php",
