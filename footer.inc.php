d68afa7724f4bf7a54f98e1fa550f66216f71e53
Gargaj <gargaj@conspiracy.hu>
2015-01-15 16:34:42 +0100

    add TUM logo

--- footer.inc.php
+++ footer.inc.php
@@ -1,6 +1,9 @@
 <footer>
   <div>
-    <div id='sceneorg'>Supported by <a href="http://www.scene.org">Scene.org</a></div>
+    <div id='supporter'>
+      Supported by <a id='sceneorg' href="http://www.scene.org">Scene.org</a>
+      and <a id='tum' href="http://www.tastatur-und-maus.net">Scene.org</a>
+    </div>
   </div>
 </footer>
 
