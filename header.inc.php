470da318dbacf1e6c14e2e8e3ecef279c50cd412
Gargaj <gargaj@conspiracy.hu>
2015-01-19 16:08:37 +0100

    more css tweaks

--- header.inc.php
+++ header.inc.php
@@ -4,13 +4,6 @@
   <title>Wanted!<?=_html($TITLE?" :: ".$TITLE:"")?></title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <link rel="stylesheet" type="text/css" href="<?=ROOT_URL?>style.css" media="screen" />
-  <!--[if IE]>
-  <style>
-    section#content div article .body {
-      word-break: break-all;
-    }
-  </style>
-  <![endif]-->
   <link rel="shortcut icon" href="<?=ROOT_URL?>favicon.ico" type="image/x-icon"/>
   <link rel="alternate" type="application/rss+xml" title="RSS" href="<?=ROOT_URL?>rss/"/>
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.2.0/prototype.js"></script>
