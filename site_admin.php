5c706728d5f387499377cee011bf9f6be376f837
Gargaj <gargaj@conspiracy.hu>
2015-02-25 17:41:08 +0100

    add more stuff to admin

--- site_admin.php
+++ site_admin.php
@@ -14,21 +14,33 @@ include_once("header.inc.php");
 
 <section id="content">
   <div>
-    <article id='about'>
+    <article id='admin' class='box'>
 
-    <h2>Admin stuff</h2>
+      <h2>Admin stuff</h2>
 
+      <div class='body'>
 <?
+echo "<h3>Message stats</h3>";
 $cntMessages = SQLLib::SelectRow("select count(*) as c from messages")->c;
 $cntUnique = SQLLib::SelectRow("SELECT count(*) as c from (select * from messages group by concat(if(userSender<userRecipient,userSender,userRecipient),':',if(userSender>userRecipient,userSender,userRecipient)) ) as m")->c;
 printf("<p><b>%d</b> messages so far, <b>%d</b> conversations</p>",$cntMessages,$cntUnique);
 
+echo "<ul>";
+$data = SQLLib::SelectRows("select count(*) as c, posts.id, posts.title from messages left join posts on posts.id = messages.relatedPost where posts.title is not null group by relatedPost order by c desc");
+foreach($data as $c)
+  printf("<li><a href='#'>%s</a>: <b>%d</b></li>",$c->title,$c->c);
+echo "</ul>";
+
+echo "<h3>Closed posts</h3>";
+echo "<ul>";
 $closure = SQLLib::SelectRows("SELECT closureReason, count(*) as c from posts group by closureReason");
 foreach($closure as $c)
   if ($c->closureReason)
-    printf("<p>%s: <b>%d</b></p>",$c->closureReason,$c->c);
-?>
+    printf("<li>%s: <b>%d</b></li>",$c->closureReason,$c->c);
+echo "</ul>";
 
+?>
+      </div>
     </article>
   </div>
 </section>
