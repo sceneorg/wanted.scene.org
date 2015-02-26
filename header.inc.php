128f2b0f7c9ea70f46e13283302458750d8c5d44
Gargaj <gargaj@conspiracy.hu>
2015-02-26 14:58:40 +0100

    add html5 shiv

--- header.inc.php
+++ header.inc.php
@@ -9,6 +9,10 @@
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.2.0/prototype.js"></script>
   <script type="text/javascript" src="<?=ROOT_URL?>calendarview.js"></script>
   <meta name="viewport" content="width=device-width; initial-scale=1.0;" />
+  
+  <!--[if lt IE 9]><script src="//ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script><![endif]-->
+  <!--[if IE]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  
 <script type="text/javascript">
 <!--
 document.observe("dom:loaded",function(){
